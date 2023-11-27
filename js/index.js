fefd8a9d8d4a8b150dba75cece3a94059e1c1c9a
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-11-27 20:28:09 +0200

    ю

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const name = 'Marina'; 
+const age = '15'+5; //255
+
+
+const productPrice = 20; 
+const quantity = 2; 
+
+let totalPrice; 
+onsole.log(totalPrice);
+
+totalPrice =  productPrice * quantity; //40
+totalPrice =  productPrice;//20
+
+// console.log(name);
+// console.log('My first js code is: ' , MyCode);
+
+// Примітивні типи даних
+const MyCode = 'Awesome';
+
+сonsole.log('7');
+сonsole.log(MyCode);
+сconsole.log(MyCode.length);
+
+const isOnline = true;
+const isOpen = false;
+console.log(typeof isOnline);
