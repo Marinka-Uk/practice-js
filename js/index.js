0e9417ed1fec9aaa5794629b9a91a2d078003b78
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-01 20:33:03 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,26 +1,59 @@
-const name = 'Marina'; 
-const age = '15'+5; //255
+// const name = 'Marina'; 
+// const age = '15'+5; //255
 
 
-const productPrice = 20; 
-const quantity = 2; 
+// const productPrice = 20; 
+// const quantity = 2; 
 
-let totalPrice; 
-onsole.log(totalPrice);
+// let totalPrice; 
+// onsole.log(totalPrice);
 
-totalPrice =  productPrice * quantity; //40
-totalPrice =  productPrice;//20
+// totalPrice =  productPrice * quantity; //40
+// totalPrice =  productPrice;//20
 
 // console.log(name);
 // console.log('My first js code is: ' , MyCode);
 
 // Примітивні типи даних
-const MyCode = 'Awesome';
+// const MyCode = 'Awesome';
 
-сonsole.log('7');
-сonsole.log(MyCode);
-сconsole.log(MyCode.length);
+// сonsole.log('7');
+// сonsole.log(MyCode);
+// сconsole.log(MyCode.length);
 
-const isOnline = true;
-const isOpen = false;
-console.log(typeof isOnline);
+// const isOnline = true;
+// const isOpen = false;
+// console.log(typeof isOnline);
+
+
+// console.log('До alert');
+// alert('Привіт користувач!');
+// console.log('після alert')
+
+// const answer = confirm('Чи дійсно ви хочете покинути сторінку?');
+// console.log(answer)
+
+// let totalOrderQuantity;
+// const orderCandies = prompt('Яку кількість цукерок ви хочете замовити?');
+// const orderCandiesNum = Number(orderCandies);
+// console.log(orderCandiesNum);
+
+// const orderCakes = prompt('Яку кількість тортів ви хочете замовити?');
+// const orderCakesNum = Number(orderCakes);
+// console.log(orderCakesNum);
+
+// totalOrderQuantity = orderCandiesNum + orderCakesNum;
+// console.log(totalOrderQuantity);
+
+// const result = prompt('Введіть що ви думаєте?');
+// console.log(result);
+
+// const str = 'Привіт';
+// const Num = 123;
+// const flag = true;
+// const txt = 'true';
+// console.log(typeof str,typeof Num,typeof flag,typeof txt)
+
+// const txt = 'Я навчаюсь в GoIteens в групі:';
+// const group = '23-14';
+// console.log(txt, group);
