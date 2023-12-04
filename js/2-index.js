6dfa41c49389c9bf4699aa2ee4323c17e591da61
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-04 20:29:49 +0200

    .

---
+++ js/2-index.js
@@ -0,0 +1,42 @@
+// let elementwidth = '50px';
+// const parsedElementWidth = Number.parseInt(elementwidth);
+// console.log(parsedElementWidth);
+// let elementHeight = '200.74px';
+
+// const salary = 1300.16472;
+// console. log(salary);
+// console. log(Number (salary. toFixed (1)));
+// console. log(salary); 
+
+// let value = newFunction();
+// console.log(Number.isNaN(value));
+
+// const minNumber = Math.min(10, 20, 2, 5);
+// console.log(minNumber);
+
+// function newFunction() {
+//     let quantity = '30';
+//     let value = 'Цей рядок неможливо перетворити в число';
+//     return value;
+// }
+
+
+/*
+ * Напиши скрипт який просить у користувача ввести число і степінь,
+ * підводить число в цю степінь і виводить результат в консоль.
+ */
+
+// Алгоритм рішення:
+// 1. Попросити користувача число - prompt() і записати значення в зміну base
+// 2. Попросити користувача число - prompt() і записати значення в зміну power
+// 3. Створити зміну і записати в неї результат підведення в степінь
+// 4. Вивести результат в консоль
+
+// const num1 = prompt('введіть число 1');
+// const num2 = prompt('введіть число 2');
+
+// const math = Math.pow(num1, num2);
+// console.log(math);
+
+const randomNumber = Math.round(Math.random() * (50 - 15) + 15)
+console.log(randomNumber);
