cdbc34a3cdead895e47a8e244be835955b233693
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-13 20:32:16 +0200

    .

---
+++ js/4-index.js
@@ -0,0 +1,36 @@
+// Рядки ''
+
+// const message = 'Я піднімала руку на уроці 8 разів. ';
+// console.log(message.length);
+// console.log(message.lastIndexOf('к'));
+
+//конкатинація
+
+//  const firstName = 'Marina ';
+//  const lastName = 'Ukrainets';
+//  const plain = 'airlines';
+//  const fullname = `${5 +5} minutes for goes ${plain}`;
+
+//  console.log(fullname);
+
+
+// const firstName = 'Marina ';
+// const lastName = 'Ukrainets';
+// const fullname = `Hello, ${firstName} ${lastName}!`;
+
+// console.log(fullname);
+
+
+//   const firstName = 'Mango';
+//   const lastName = 'Lee';
+//   const room = 716;
+//   const type = 'VIP';
+// const customersMsg = `Гість ${firstName} ${lastName} заселяється в ${room} номер ${type}` ;
+// console.log (customersMsg)
+
+
+// const firstName = prompt ( 'Your name');
+// const lastName = prompt( 'Your surname');
+// const age = prompt( 'Your age');
+// const text = `Користувач ${firstName} ${lastName} віком ${age} має доступ до аккаутну!`;
+// console.log(text);
