955f5b38e38d365d6fcd141c2c2e5383c0822926
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-25 20:28:28 +0200

    ю

---
+++ js/6-index.js
@@ -0,0 +1,124 @@
+//Тернарний оператор
+
+
+// let message;
+// const userName = 'Poly';
+
+// if(userName === 'Poly'){
+//     message = 'Hello, Poly'
+// } else {
+//     message = 'Unknown userName'
+// }
+//message = userName === 'Poly' ? 'Hello, Poly' : 'Unknown userName' ;
+//Тернарний оператор - умова ? правдива умова : якщо умова неправдива
+
+//console.log(message); //'Hello, Poly'
+
+
+
+/*
+ * Напиши скрипт вибору готеля по кількості зірок.
+ * 1 - 10$, 
+ * 2 - 20$,  
+ * 3 - 30$
+ * 4 - 50$
+ * 5 - 120$
+ * 
+ * Якщо в змінній stars щось окрім чисел 1-5, виведи рядок
+ * 'Токої кількості зірок немає'
+ */
+
+ //const stars = 3;
+ //let price;
+
+//  if ( stars === 1){
+//     price = 10
+//     } else if (stars === 2){
+//     price = 20
+//     } else if (stars === 3){
+//     price = 30
+//     } else if (stars === 4){
+//     price = 50
+//     } else if (stars === 5){
+//     price = 120
+//     } else {
+//         console.log('Такої кількості зірок немає');
+//     }
+
+
+    //console.log(`Ціна за номер в готелі ${price}$`);
+
+
+// switch (stars) {
+//     case 1:
+//         price = 10
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break;
+//     case 2:
+//         price = 20
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break;
+//     case 3:
+//         price = 30
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break;
+//     case 4:
+//             price = 50
+//             console.log(`Ціна за номер в готелі ${price}$`);
+//             break;
+//     case 5:
+//                 price = 120
+//                 console.log(`Ціна за номер в готелі ${price}$`);
+//                 break;       
+
+//           default:
+//             console.log('такоїї кількості зірок немає');     
+//             break;     
+// }
+
+
+
+// const a = 5;
+// if(true){
+//     const b = 10;
+//     const a = 15;
+//     console.log(b);
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+//Перепишіть код нижче використовуючи одну конструкцію switch. 
+//Результат виведіть на екран. Данні отримайте з інпуту:
+//let a = значення інпуту;
+
+
+// if (a === 0) {
+//    p.textContent = 0;
+// }
+// if (a === 1) {
+//   p.textContent = 1;
+// }
+
+
+// if (a === 2 || a === 3) {
+//   p.textContent = '2,3';
+// }
+
+
+let a = Number(prompt('Значення'));
+switch (a) {
+    case 0:
+        console.log(0);
+        break;
+    case 1:
+            console.log(1);
+            break;
+    case 2 || 3:
+            console.log('2,3');
+             break;
+    default:
+        console.log(' default');
+        break;
+}
