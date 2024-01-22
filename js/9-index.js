d02711e7d73ca8875fd2b1bfdc751833a15ec058
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-01-22 20:24:06 +0200

    .

---
+++ js/9-index.js
@@ -0,0 +1,39 @@
+//Функції
+
+//Function expression
+
+// const fnA = function (a, b){
+//     console.log(a);
+//     console.log(b);
+//     console.log('hello');
+// }
+
+// fnA(5, 6);
+// fnA(14, 59);
+// fnA(124, 52);
+
+
+// const fnA = function (a, b){
+//   return a + b;
+// }
+
+// const result1 = fnA(5, 10 )
+// console.log(result1);
+
+// const result2 = fnA(15, 20 )
+// console.log(result2);
+
+// const result3 = fnA(45, 160 )
+// console.log(result3);
+
+
+
+/*
+ * Напиши функцию slugify(string) яка отримує рядок і повертає URL-slug
+ * Заголовок статті складається тільки з букв та пропусків
+ */
+
+// const slugify = function (string){
+//     const slugTitle = string.toLoaleLowerCase().split(' ').join('-');
+
+// }
