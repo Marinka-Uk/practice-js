34a5bf396b2ee3a41281e1abc214cec260e11628
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-01-29 20:22:55 +0200

    .

--- js/9-index.js
+++ js/9-index.js
@@ -95,3 +95,67 @@
 // console.log(findLogin(logins, 'k1widab3st'));
 // console.log(findLogin(logins, 'jam4l'));
 // console.log(findLogin(logins, 'poly1scute'));
+
+
+
+//*
+//псевдомасив arguments
+
+
+// const calculateTotalPrice = function (){
+// console.log(arguments);
+// const args = Array.from(arguments)
+// console.log(args);
+
+// let total = 0;
+// for(let hgh of args){
+// total += hgh
+// }
+// return total;
+// }
+
+// const result = calculateTotalPrice(1, 2, 3 );
+// console.log(result);
+// calculateTotalPrice(5, 12, 33 );
+// calculateTotalPrice(9, 245, 39 );
+
+
+// const calculateTotalPrice = function (...args){
+// console.log(args);
+//     let total = 0;
+
+//     for(let mdrm of args){
+//         total +=mdrm
+    
+//     }
+//     return total;
+// }
+// const result = calculateTotalPrice(1, 2, 3);
+// console.log(result);
+
+
+
+/*
+     * Напиши функцію filterNumbers(array [, number1, ...]) яка:
+     * - першим аргументом приймає масив чисел
+     * - після першого аргумента може бути довільна кількість інших аргументів які будуть числами,
+     * - Функція повина повернути новий масив, в якому будуть тільки ті аргументи, починаючи з другого,
+     *   для яких є аналог в оригінальному масив.
+     */
+
+// const filterNumbers = function (array, ...args){
+//     const  equalNumbers = [];
+//     for(let number of args){
+//       const isEqual =  array.includes(number);
+//         if(isEqual){
+//             equalNumbers.push(number)
+//         }
+//     }
+//     return equalNumbers;
+//}
+
+
+
+// console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
+    // console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
+    // console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
