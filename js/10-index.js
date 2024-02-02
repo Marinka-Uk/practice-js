908928399bb45c3fbf3b33466cd90eeef4a5a6d9
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-02-02 20:23:32 +0200

    .

---
+++ js/10-index.js
@@ -0,0 +1,63 @@
+//basics
+
+//посилальний тип даних
+
+// let number = 5;
+// const number2 = number;
+
+// number = 25;
+
+// console.log(number);
+// console.log(number2);
+
+// // console.log(number === number2);
+// // console.log('Hello' === 'Hello');
+
+// const arr1 = [];
+// const arr3 = arr1;
+
+// arr1.push(10)
+// console.log(arr1);
+// console.log(arr3);
+
+
+// console.log(arr1 === arr3);
+// console.log([1, 2, 3] === [1, 2, 3]);
+
+//Callback function
+
+// const getUserName = function(name){
+//     console.log(`hello ${name}`);
+// }
+
+
+// const logName = function(userName, cb){
+//     cb(userName)
+// }
+
+// logName('Mary', getUserName)
+
+
+/*
+//стрілкова функція
+/*/
+
+// const fnA = function (){}
+//  function name(){}
+
+//  const fnB = (name)=>{
+//     console.log(`Hi, ${name}!`);
+//  }
+
+//  fnB('Poly')
+
+
+// const logItems = (items)=>{
+//         for (let i of items){
+//     console.log(i);
+//         }
+//     }
+    
+//     logItems(['Mango', 'Kiwi', 'Poly', 'Ajax']);
+// logItems([1, 2, 3, 4, 5]);
+// logItems(['клавиатура', 'наушники', 'часы']);
