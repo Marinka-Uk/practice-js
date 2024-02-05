27ccddb98519a60edfd9cca5cb9abc68286d7390
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-02-05 20:25:11 +0200

    .

---
+++ js/11-index.js
@@ -0,0 +1,10 @@
+// const person = {
+//     name : 'Marina',
+//     age : 15,
+//     gender : 'female'
+// } ;
+// person.age += 6;
+// person.university = 'Holywood';
+// console.log(person);
+
+
