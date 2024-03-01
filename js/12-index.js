174dcf2af1f8c6131770f37952f731d13cdcf512
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-03-01 20:14:25 +0200

    .

--- js/12-index.js
+++ js/12-index.js
@@ -24,17 +24,60 @@
 
 
 
-const numbers = [5, 10, 15, 20, 25] ;
-let total = 0;
+// const numbers = [5, 10, 15, 20, 25] ;
+// let total = 0;
 
-numbers.forEach(function(number){
-    if(number % 2 === 0){
-        total += number;
-    }
-})
+// numbers.forEach(function(number){
+//     if(number % 2 === 0){
+//         total += number;
+//     }
+// })
 
 
-console.log(total);
+// console.log(total);
 
 //10 % 3 = 1
 //20 % 5 = 0
+
+
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const doubleNums = numbers.map((number) => {
+//     return number * 2
+// })
+
+
+
+// console.log('numbers', numbers);
+// console.log('doubleNums', doubleNums);
+
+
+
+
+/*
+* Збільшуємо кількість поінтів кожного гравця на 10 (розпорошуємо старий об'єкт)
+// */
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+    // ];
+    // console.table(players);
+
+
+//     const upatedPlayers = players.map((player) => {
+// return {
+//     ...player,
+//     points: player.points + 10
+// }
+//     })
+
+//     console.log(upatedPlayers);
+
+
+
+
