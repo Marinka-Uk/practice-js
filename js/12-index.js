42ef9600cbc99b9b8bf81c97646f7b4676f0c06d
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-03-04 20:25:17 +0200

    .

--- js/12-index.js
+++ js/12-index.js
@@ -81,3 +81,54 @@
 
 
 
+// const numbers = [5, 10, 15, 20, 25];
+// const filteredNubbers = numbers.filter((number) =>
+// {
+//   return number <= 10
+// })
+// console.log(filteredNubbers);
+
+
+
+const players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+    ];
+
+//     const getOnlinePlayers = function(players){
+//     const OnlinePlayers = players.filter((player) => {console.log(player);
+//         return !player.online   
+    
+//     }) 
+
+//     console.log(OnlinePlayers);
+// }
+
+// getOnlinePlayers(players)
+
+
+
+// const hardcorePlayers = players.filter(({timePlayed})=>{
+// return timePlayed > 250
+// })
+
+// console.table(hardcorePlayers);
+
+
+
+
+//метод find
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const number = numbers.find((number)=>{return number === 15})
+// console.log(number);
+
+// const findplayerById = function (playersArr, playerId){
+//    return playersArr.find((player)=>{return player.id === playerId})
+// }
+// console.log(findplayerById(players, 'player-1'));
