afa0fdac0ca603c6e1c757dba87625222ac9ff0d
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-03-22 20:24:16 +0200

    .

---
+++ js/13-index.js
@@ -0,0 +1,31 @@
+//DOM
+
+
+/*
+* Інтерфейс classList
+* - add(клас)
+* - remove(клас)
+* - toggle(клас)
+* - replace(старийКлас, новийКлас)
+* - contains(клас)
+*/
+
+
+// const navlistRef = document.querySelector('ul')
+// navlistRef.classList.add('nav-list')
+// navlistRef.classList.add('js-nav-list')
+
+// navlistRef.classList.remove('js-nav-list')
+
+// navlistRef.classList.toggle('nav-list')
+// navlistRef.classList.toggle('nav-list')
+// navlistRef.classList.toggle('js-nav-list')
+
+// navlistRef.classList.replace('js-nav-list', 'new-class')
+
+// console.log(navlistRef.classList.contains('new-class'));
+// console.log(navlistRef.classList);
+
+
+
+
