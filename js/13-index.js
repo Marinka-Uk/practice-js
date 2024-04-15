6409b30fa02f66c7fb804a6c6208dfaff2eaa9bc
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-04-15 20:22:59 +0300

    ,

--- js/13-index.js
+++ js/13-index.js
@@ -164,16 +164,49 @@
 
 
 
-const openModalBtn = document.querySelector('[data-action="open-modal"]');
-const closeModalBtn = document.querySelector('[data-action="close-modal"]');
-const backdrop = document.querySelector('.js-backdrop');
+// const openModalBtn = document.querySelector('[data-action="open-modal"]');
+// const closeModalBtn = document.querySelector('[data-action="close-modal"]');
+// const backdrop = document.querySelector('.js-backdrop');
 
 
 
-openModalBtn.addEventListener('click', onOpenModal)
-function onOpenModal() {
-document.body.classList.add('show-modal')
+// openModalBtn.addEventListener('click', onOpenModal)
+// function onOpenModal() {
+// document.body.classList.add('show-modal')
 
-}
+// }
+
+
+
+
+
+const colorPickerOptions = [
+    { label: 'red', color: '#F44336' },
+    { label: 'green', color: '#4CAF50' },
+    { label: 'blue', color: '#2196F3' },
+    { label: 'grey', color: '#607D8B' },
+    { label: 'pink', color: '#E91E63' },
+    { label: 'indigo', color: '#3F51B5' },
+    ];
+    
+    const colorPickerContainerEl = document.querySelector('.js-color-picker');
+    const option = colorPickerOptions[0];
+    const btnEl = document.createElement('buton');
+    btnEl.type = 'button';
+    btnEl.textContent = option.color;
+    btnEl.style.backgroundColor = option.color;
+
+    const btnElList = colorPickerOptions.map(option => {
+        const btnEl = document.createElement('buton');
+    btnEl.type = 'button';
+    btnEl.textContent = option.label;
+    btnEl.style.backgroundColor = option.color;
+
+    return btnEl
+    })
+    console.log(...btnElList);
+    colorPickerContainerEl.append(...btnElList)
+
+
 
 
