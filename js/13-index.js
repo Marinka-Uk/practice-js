c42ddc7a6cbe7d84801e58547b88a4d27a00ea57
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-04-19 19:12:33 +0300

    ю

--- js/13-index.js
+++ js/13-index.js
@@ -180,32 +180,32 @@
 
 
 
-const colorPickerOptions = [
-    { label: 'red', color: '#F44336' },
-    { label: 'green', color: '#4CAF50' },
-    { label: 'blue', color: '#2196F3' },
-    { label: 'grey', color: '#607D8B' },
-    { label: 'pink', color: '#E91E63' },
-    { label: 'indigo', color: '#3F51B5' },
-    ];
+// const colorPickerOptions = [
+//     { label: 'red', color: '#F44336' },
+//     { label: 'green', color: '#4CAF50' },
+//     { label: 'blue', color: '#2196F3' },
+//     { label: 'grey', color: '#607D8B' },
+//     { label: 'pink', color: '#E91E63' },
+//     { label: 'indigo', color: '#3F51B5' },
+//     ];
     
-    const colorPickerContainerEl = document.querySelector('.js-color-picker');
-    const option = colorPickerOptions[0];
-    const btnEl = document.createElement('buton');
-    btnEl.type = 'button';
-    btnEl.textContent = option.color;
-    btnEl.style.backgroundColor = option.color;
+//     const colorPickerContainerEl = document.querySelector('.js-color-picker');
+//     const option = colorPickerOptions[0];
+//     const btnEl = document.createElement('buton');
+//     btnEl.type = 'button';
+//     btnEl.textContent = option.color;
+//     btnEl.style.backgroundColor = option.color;
 
-    const btnElList = colorPickerOptions.map(option => {
-        const btnEl = document.createElement('buton');
-    btnEl.type = 'button';
-    btnEl.textContent = option.label;
-    btnEl.style.backgroundColor = option.color;
+//     const btnElList = colorPickerOptions.map(option => {
+//         const btnEl = document.createElement('buton');
+//     btnEl.type = 'button';
+//     btnEl.textContent = option.label;
+//     btnEl.style.backgroundColor = option.color;
 
-    return btnEl
-    })
-    console.log(...btnElList);
-    colorPickerContainerEl.append(...btnElList)
+//     return btnEl
+//     })
+//     console.log(...btnElList);
+//     colorPickerContainerEl.append(...btnElList)
 
 
 
